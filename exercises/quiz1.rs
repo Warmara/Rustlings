3c3f457fa1a89d9d7ed870d0dfe4ce80f454067d
Marek Jína <makula96@gmail.com>
2023-11-13 16:05:39 +0200

    Restore init

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (number: i32) -> i32 {
+    if number > 40 {
+        return number * 1
+    } else {
+        return number * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
